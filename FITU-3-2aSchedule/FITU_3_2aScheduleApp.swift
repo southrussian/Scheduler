d58c0f5d15af4a61d70746428985ede7a430c8f2
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-02-21 00:51:12 +0300

    Initial Commit

---
+++ FITU-3-2aSchedule/FITU_3_2aScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FITU_3_2aScheduleApp.swift
+//  FITU-3-2aSchedule
+//
+//  Created by Danil Peregorodiev on 21.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct FITU_3_2aScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
